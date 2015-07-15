18fbac94dd1791b11abfff7de39c28b85eee0efc
brandon932 <bthornton_932@yahoo.com>
2015-07-15 15:48:19 -0600

    finished working fizzBuzz program

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-div = 0;
-for (var i = 0; i < 100 ; i++) {
-div = div + 1;
-if (div % 3 && div % 5 === 0 ){
+for (var i = 1; i < 100 ; i++) {
+if (i % 3 === 0 && i % 5 === 0 ){
   console.log("Fizz Buzz");
-} else if (div % 3 === 0){
-  console.log("Fizz");
-} else if (div % 5 === 0) {
- console.log("Buzz");
+} else if (i % 5 === 0){
+  console.log("Buzz");
+} else if (i % 3 === 0) {
+ console.log("Fizz");
 }
 else {
-console.log(div);
+console.log(i);
 }
 }
